72fa5e85840002314a630f1f39d45ade382765c8
Baji Shaik <you@example.com>
2025-05-15 15:46:15 +0530

    playing with routehandlers and next parameters

--- src/app.js
+++ src/app.js
@@ -1,54 +1,35 @@
 const express = require("express");
 
 const app = express();
-//b optional
-// app.get(/\/ab?c/, (req, res) => {
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
-//any no of b's
-// app.get(/\/ab+c/, (req, res) => {
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
 
-//anything in between ab and c
-// app.get("/ab*c", (req, res) => {
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
-
-//grouping
-// app.get(/\/a(bc)+d/, (req, res) => {
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
-
-//anything which is having in a
-// app.get(/a/, (req, res) => {
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
-
-//anything which matches pattern
-// app.get(/.*fly$/, (req, res) => {
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
-
-//query params
-// app.get("/user", (req, res) => {
-//   console.log(req.query);
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
-
-//dynamic routing
-app.get("/user/:userid", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Baji", lastName: "Shaik" });
-});
-
-// app.get("/user", (req, res) => {
-//   res.send({ firstName: "Baji", lastName: "Shaik" });
-// });
-
-// app.use("/test", (req, res) => {
-//   res.send("Hello from test");
-// });
+app.use(
+  "/user",
+  [(req, res,next) => {
+    console.log("Handling the router User");
+    next();
+    // res.send("Response !!!");
+  },
+  (req, res,next) => {
+    console.log("Handling the route user 2");
+    // res.send("Response 2!!!");
+    next();
+  }],
+  (req, res,next) => {
+    console.log("Handling the route user 3");
+    // res.send("Response 3!!!");
+    next();
+  },
+  (req, res,next) => {
+    console.log("Handling the route user 4");
+    // res.send("Response 4!!!");
+    next();
+  },
+  [(req, res,next) => {
+    console.log("Handling the route user 5");
+    res.send("Response 5!!!");
+    // next();
+  }]
+);
 
 app.listen("7777", () => {
   console.log("Server started successfully on port 7777....");
