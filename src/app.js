0155edd44f4254a02fea0f6ebed196bc71e6c87e
Baji Shaik <you@example.com>
2025-05-15 16:08:41 +0530

    lingo of middleware and routehandler

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res, next) => {
-  console.log("Handling the route user");
+app.use("/", (req, res, next) => {
+  //this is called as middleware
   next();
 });
 
-app.use("/user", (req, res) => {
-  console.log("Handling the route user2");
-  res.send("Handled!!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // this is also middleware
+    next();
+  },
+  (req, res, next) => {
+    // this is also middleware
+    next();
+  },
+  (req, res) => {
+    // this is route handler as it is sending the response back to client
+    console.log("Handling the route user2");
+    res.send("Handled!!");
+  }
+);
 
 app.listen("7777", () => {
   console.log("Server started successfully on port 7777....");
