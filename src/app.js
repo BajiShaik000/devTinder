b963ba31fe7da9bf611b4a09c15a37c11c2e1dd8
Baji Shaik <you@example.com>
2025-05-15 12:58:30 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello from hello");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Baji", lastName: "Shaik" });
+});
+
+app.post("/user", (req, res) => {
+  //save to db
+  res.send("saved to db successfully");
+});
+
+app.patch("/user", (req, res) => {
+  //update in db
+  res.send("Updated in db successfully");
+});
+
+app.delete("/user", (req, res) => {
+  //delete from db
+  res.send("deleted from db successfully");
 });
 
 app.use("/test", (req, res) => {
   res.send("Hello from test");
-});
-
-app.use("/", (req, res) => {
-  res.send("Namaste Node.js");
 });
 
 app.listen("7777", () => {
