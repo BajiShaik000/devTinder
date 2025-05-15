4b19bdfacbd5f4e1cc341db2b20d014900027a24
Baji Shaik <you@example.com>
2025-05-15 16:06:31 +0530

    another way of handling routes

--- src/app.js
+++ src/app.js
@@ -2,34 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  [(req, res,next) => {
-    console.log("Handling the router User");
-    next();
-    // res.send("Response !!!");
-  },
-  (req, res,next) => {
-    console.log("Handling the route user 2");
-    // res.send("Response 2!!!");
-    next();
-  }],
-  (req, res,next) => {
-    console.log("Handling the route user 3");
-    // res.send("Response 3!!!");
-    next();
-  },
-  (req, res,next) => {
-    console.log("Handling the route user 4");
-    // res.send("Response 4!!!");
-    next();
-  },
-  [(req, res,next) => {
-    console.log("Handling the route user 5");
-    res.send("Response 5!!!");
-    // next();
-  }]
-);
+app.use("/user", (req, res, next) => {
+  console.log("Handling the route user");
+  next();
+});
+
+app.use("/user", (req, res) => {
+  console.log("Handling the route user2");
+  res.send("Handled!!");
+});
 
 app.listen("7777", () => {
   console.log("Server started successfully on port 7777....");
