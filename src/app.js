7d5ecc21d6cf7679b4d1cdaf043b3f5eb78271ff
Baji Shaik <you@example.com>
2025-05-15 13:52:14 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,29 +1,54 @@
 const express = require("express");
 
 const app = express();
+//b optional
+// app.get(/\/ab?c/, (req, res) => {
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
+//any no of b's
+// app.get(/\/ab+c/, (req, res) => {
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
 
-app.get("/user", (req, res) => {
+//anything in between ab and c
+// app.get("/ab*c", (req, res) => {
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
+
+//grouping
+// app.get(/\/a(bc)+d/, (req, res) => {
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
+
+//anything which is having in a
+// app.get(/a/, (req, res) => {
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
+
+//anything which matches pattern
+// app.get(/.*fly$/, (req, res) => {
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
+
+//query params
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
+
+//dynamic routing
+app.get("/user/:userid", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Baji", lastName: "Shaik" });
 });
 
-app.post("/user", (req, res) => {
-  //save to db
-  res.send("saved to db successfully");
-});
+// app.get("/user", (req, res) => {
+//   res.send({ firstName: "Baji", lastName: "Shaik" });
+// });
 
-app.patch("/user", (req, res) => {
-  //update in db
-  res.send("Updated in db successfully");
-});
-
-app.delete("/user", (req, res) => {
-  //delete from db
-  res.send("deleted from db successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from test");
-});
+// app.use("/test", (req, res) => {
+//   res.send("Hello from test");
+// });
 
 app.listen("7777", () => {
   console.log("Server started successfully on port 7777....");
